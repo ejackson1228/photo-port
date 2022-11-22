6cecbda29881f362167c6b0a1e7f35d3fef8f9f7
ejackson1228 <ejackson1228@gmail.com>
2022-11-22 14:33:06 -0500

    added conditional rendering for contact form, about, and gallery as well as conditional styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false); //false prevents the contact form from showing on initial page navigation 
+  
   const [categories] = useState([
     {
       name: 'commercial',
@@ -20,15 +22,22 @@ function App() {
 
   return (
     <div>
-      <Nav
+      <Nav // passing  these getter and setter functions will allow this component to modify the state in the APP component, which will conditionally render based on selection
         categories = {categories}
         setCurrentCategory = {setCurrentCategory}
         currentCategory  = {currentCategory}
+        contactSelected = {contactSelected}
+        setContactSelected = {setContactSelected}
         ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? ( // if contact is not selected, display gallery and about
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : ( // else display contact form 
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
-    const [ formState, setFormState ] = useState({ name: '', email: '', message: ''});
+    const [ formState, setFormState ] = useState({ name: '', email: '', message: ''}); // set initial states to empty strings
     const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
 
     function handleChange(e)  {
-        setFormState({...formState, [e.target.name]: e.target.value })
+        if(e.target.name ==='email') {
+        const isValid = validateEmail(e.target.value); //isValid returns a boolean
+        console.log(isValid);
+        // isValid conditional statement
+        if(!isValid) {
+            setErrorMessage('Your email is invalid!');
+        } else {
+            setErrorMessage('');
+        }
+        } else {
+            if(!e.target.value.length) { //if the field is empty, set error message to 
+                setErrorMessage(`${e.target.name} is required.`); // this 
+            } else {
+                setErrorMessage(''); // set error message to empty string 
+            }
+        }
+        setFormState({...formState, [e.target.name]: e.target.value }) // use the spread operator to prevent the other values from changing (i.e. email or message)
+
+        if(!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value }); // if there's not error message, set the new state to the value of the target
+        }
     };
 
     function handleSubmit(e) {
@@ -13,22 +35,29 @@ function ContactForm() {
         console.log(formState);
     }
 
+    //onBlur attribute only acts when the target is no longer in focus 
+
     return (
         <section>
             <h1>Contact Me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} /> 
                 </div>
                 <div>
                     <label htmlFor="email">Email address</label>
-                    <input type="email" name="email" defaultValue={email} onChange={handleChange}/>
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}/>
                 </div>
+                {errorMessage &&  ( // similar to an if statement
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
