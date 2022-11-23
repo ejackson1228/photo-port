9796b44b7a69f47c964c16f61404bd90acc9cdbd
ejackson1228 <ejackson1228@gmail.com>
2022-11-23 12:37:32 -0500

    created modal for rednering close-up of gallery images. modal can open but not close.

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import Modal from "../Modal";
 
 const PhotoList = ({ category }) => {
-    const [photos] = useState([
+  const [isModalOpen, setIsModalOpen] = useState(false); //false sets the initial state of modal to not open yet
+  
+  const [photos] = useState([
         {
             name: 'Grocery Aisle',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
@@ -98,23 +101,33 @@ const PhotoList = ({ category }) => {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
           },
     ]);
-    
+
     const currentPhotos = photos.filter((photo) => photo.category === category);
 
+    const [currentPhoto, setCurrentPhoto] = useState();
+
+    const toggleModal = (image, i) => {
+      setCurrentPhoto({...image, index: i});
+      setIsModalOpen(true);
+    };
+
+    
     return (
-        <div>
-            <div className="flex-row">
-                {currentPhotos.map((image, i) => (
-                    <img 
-                    src={require(`../../assets/small/${category}/${i}.jpg`)}
-                    alt={image.name}
-                    className = 'img-thumbnail mx-1'
-                    key={image.name}
-                    />
-                ))}
-            </div>
+      <div>
+        {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+        <div className="flex-row">
+          {currentPhotos.map((image, i) => (
+            <img
+              src={require(`../../assets/small/${category}/${i}.jpg`)}
+              alt={image.name}
+              className="img-thumbnail mx-1"
+              onClick={() => toggleModal(image, i)}
+              key={image.name}
+            />
+          ))}
         </div>
-    )
+      </div>
+    );
 };
 
 export default PhotoList;
